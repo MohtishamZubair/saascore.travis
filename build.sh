07c2110520e4791ac15333d2e27d1680ced329c7
mohtisham <mohtishamzubair@gmail.com>
2017-04-06 01:07:10 +0500

    set the steps
    build the the travis

---
+++ build.sh
@@ -0,0 +1,15 @@
+﻿#!/usr/bin/env bash
+# using from MaxMind-DB-Reader-dotnet
+
+cd `dirname $0`/..
+
+if [ -n "$DOTNETCORE" ]; then
+
+  echo Using .NET CLI
+
+  dotnet restore
+
+  # Running Benchmark
+  dotnet run -f netcoreapp1.0 -c $CONFIGURATION -p ./src/SaasCore.Web/SaasCore.Web.xproj
+ 
+fi
