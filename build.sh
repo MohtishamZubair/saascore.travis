c4e4e869af155bdeff6c9e289d6369d4e8890996
mohtisham <mohtishamzubair@gmail.com>
2017-04-06 02:18:35 +0500

    trying

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
-﻿#!/usr/bin/env bash
-# using from MaxMind-DB-Reader-dotnet
+﻿#!/bin/bash
+set -ev
 
-cd `dirname $0`/..
+# ---------------------------------
+# BUILD
+# ---------------------------------
 
-if [ -n "$DOTNETCORE" ]; then
-
-  echo Using .NET CLI
-
-  dotnet restore
-
-  # Running Benchmark
-  dotnet run -f netcoreapp1.0 -c $CONFIGURATION -p ./src/SaasCore.Web/SaasCore.Web.xproj
- 
-fi
+dotnet restore
+dotnet build -c Release 
